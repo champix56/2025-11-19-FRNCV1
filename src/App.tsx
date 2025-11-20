032f423b6aa12a155a67cd575049a771e471f2a8
champix56 <desorbaix@free.fr>
2025-11-20 15:01:47 +0100

    demo state local et fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,42 @@
  * @format
  */
 
-import {
-  Text,
-  useColorScheme,
-  View,
-  Button,
-  ScrollView,
-  FlatList,
-} from 'react-native';
+import { Text, View, ScrollView, Button } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import ProductListItem from './components/ProductListItem/ProductListItem';
-import { products } from '../db.json';
+
+import { useEffect, useState } from 'react';
+import { IProductListItem } from './interfaces/IProducts';
+import { RESSOURCES_NAMES, REST_ADR } from './config/constantes';
 
 function App() {
+  const [products, setproducts] = useState<Array<IProductListItem>>([]);
+  useEffect(() => {
+    fetch(`${REST_ADR}${RESSOURCES_NAMES.PRODUCTS}`)
+      .then(r => r.json())
+      .then(data => {
+        setproducts(data);
+      })
+      .catch(e => console.log(e));
+    return () => {};
+  }, []);
   return (
     <SafeAreaProvider>
       {/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} /> */}
       <View style={{ paddingTop: 25 }}>
-        <Text style={{TextAlign:'center', fontSize:18}}>Liste des produits</Text>
+      <Button title="add empty product"  onPress={() => {
+          setproducts([...products, { name: 'Produit vide', prix: 0, stock: 0, img: '', id:Math.random()*100000 }]);
+        }} />
+        <Button title="Coucou" onPress={() => alert('Coucou')} />
+        <Text style={{ textAlign: 'center', fontSize: 18 }}>
+          Liste des produits
+        </Text>
         <ScrollView style={{ height: 350 }}>
-          {
-            products.map((item,position)=><ProductListItem produit={item} key={item.id}/>)
-          }
+          {products.map(item => (
+            <ProductListItem produit={item} key={item.id} />
+          ))}
         </ScrollView>
+       
       </View>
     </SafeAreaProvider>
   );
