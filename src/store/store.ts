1a4497ace6253053794e43684d30c8a65de1b583
champix56 <desorbaix@free.fr>
2025-11-20 15:52:40 +0100

    manipulation du store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,51 +1,26 @@
-import { IProductListItem } from '../interfaces/IProducts';
-import { products } from '../../db.json';
 import { configureStore } from '@reduxjs/toolkit';
+import stockReducer, { addProductToStock } from './stock';
+import {products} from '../../db.json'
+const store = configureStore({
+  reducer: stockReducer,
+});
 
-interface IStoreState {
-  counter: number;
-  products: Array<IProductListItem>;
-}
-const initialState: IStoreState = {
-  counter: 0,
-  products: products,
-};
 
-const reducer = (
-  state = initialState,
-  action: { type: string; payload: any },
-) => {
-  switch (action.type) {
-    case 'RESET_PRODUCT_LIST':
-      return { ...state, products: [] };
-    case 'DELETE_PRODUCT_IN_LIST_BY_ID':
-      const position = state.products.findIndex(p => p.id === action.payload);
-      console.log(position);
-      if (position > -1) {
-        return state;
-      }
-      return {
-        ...state,
-        products: [
-          ...state.products.slice(0, position-1),
-          ...state.products.slice(position + 1),
-        ],
-      };
 
-    default:
-      return state;
-  }
-};
 
-//constructeur d'actions
-export const deleteProductInListById = (id: number) => {
-  return { type: 'DELETE_PRODUCT_IN_LIST_BY_ID', payload: id };
-};
 
-const store = configureStore({ reducer:reducer, devTools:true });
+export default store;
+//typage du store(states et dispatch) invariable
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+
 
 store.subscribe(() => {
-  console.log('store changed : ', store.getState());
+  console.log('Store updated : ', store.getState());
 });
-store.dispatch(deleteProductInListById(2));
-store.dispatch(deleteProductInListById(3));
+
+
+store.dispatch(addProductToStock(products[0]));
+store.dispatch(addProductToStock(products[1]));
+store.dispatch(addProductToStock(products[2])); 
