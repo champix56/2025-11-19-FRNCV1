b3d41b4cadbcad06650f74ed471d1e45ce61d5dd
champix56 <desorbaix@free.fr>
2025-11-20 10:45:12 +0100

    corriger exo 1

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,31 @@ function App() {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={{ marginTop: 10, paddingVertical: 5 }}>
         <ProductListItem
-          produit={{ id: 1, name: '', img: '', prix: 3.99, stock: 6 }}
-        ></ProductListItem>
+          produit={{
+            id: 1,
+            name: 'coca zero 33cl',
+            img: 'https://www.bieres64-40.fr/wp-content/uploads/2022/12/cocazero33.png',
+            prix: 1.99,
+            stock: 6,
+          }}
+        />
+        <ProductListItem
+          produit={{
+            id: 2,
+            name: 'Kouign aman',
+            img: 'https://biscuiterie-lafabrique.fr/wp-content/uploads/2024/09/Kouign-amann-praline-rose-La-Fabrique-Chavagne-e1747217497199.png',
+            prix: 3.85,
+            stock: 0,
+          }}
+        /><ProductListItem
+          produit={{
+            id: 3,
+            name: 'beurre salé',
+            img: 'https://dojwn62xby8qn.cloudfront.net/public/offer_presentations/slideshows/original/16389-1640171519.png?1640171519',
+            prix: 4.85,
+            stock: 2,
+          }}
+        />
       </View>
     </SafeAreaProvider>
   );
--- src/components/ProductListItem/ProductListItem.tsx
+++ src/components/ProductListItem/ProductListItem.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import React from 'react';
 interface IProductListItem {
   id?: number;
@@ -7,15 +7,48 @@ interface IProductListItem {
   stock: number;
   img: string;
 }
-interface IProductListItemProps{
-    produit:IProductListItem
+interface IProductListItemProps {
+  produit: IProductListItem;
 }
-const ProductListItem: React.FC<IProductListItemProps> = props => {
+const ProductListItem: React.FC<IProductListItemProps> = ({ produit }) => {
   return (
-    <View>
-      <Text>ProductListItem</Text>
+    <View style={style.container}>
+      <View style={style.imageContainer}>
+        <Image source={{ uri: produit.img }} style={style.image} />
+      </View>
+      <View style={style.textContainer}>
+        <Text style={style.text}>Nom:<Text style={style.textValue}>{produit.name}</Text></Text>
+        <Text style={style.text}>Prix:<Text style={style.textValue}>{produit.prix}€</Text></Text>
+        <Text style={style.text}>Stock:<Text style={style.textValue}>{produit.stock}</Text></Text>
+      </View>
     </View>
   );
 };
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderColor:'grey',
+    borderWidth:2,
+    borderRadius:5,
+    marginBottom:5
+  },
+  imageContainer: {
+    padding:5
+  },
+  textContainer: {
+    flexGrow: 1,
+    paddingVertical:5,
+    justifyContent:'space-between'
+  },
+  image: {
+    width: 96,
+    height: 96,
+  },
+  text:{
 
+  },
+  textValue:{
+    fontWeight:"900"
+  }
+});
 export default ProductListItem;
