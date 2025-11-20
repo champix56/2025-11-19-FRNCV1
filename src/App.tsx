16a723ca5efffb7e977dd88355e6b2412eb69282
champix56 <desorbaix@free.fr>
2025-11-20 11:53:15 +0100

    demo non fonctionnel sans etat

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,62 @@
  * @format
  */
 
-import { Alert, StatusBar, Text, useColorScheme, View } from 'react-native';
+import {
+  Alert,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+} from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Button from './components/Button/Button';
-import buttonStyle from './components/Button/Button.style';
-import ProductListItem from './components/ProductListItem/ProductListItem';
+//import ProductListItem from './components/ProductListItem/ProductListItem';
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-
+  let counter = 0;
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={{ marginTop: 10, paddingVertical: 5 }}>
-        <ProductListItem
-          produit={{
-            id: 1,
-            name: 'coca zero 33cl',
-            img: 'https://www.bieres64-40.fr/wp-content/uploads/2022/12/cocazero33.png',
-            prix: 1.99,
-            stock: 6,
-          }}
-        />
-        <ProductListItem
-          produit={{
-            id: 2,
-            name: 'Kouign aman',
-            img: 'https://biscuiterie-lafabrique.fr/wp-content/uploads/2024/09/Kouign-amann-praline-rose-La-Fabrique-Chavagne-e1747217497199.png',
-            prix: 3.85,
-            stock: 0,
-          }}
-        /><ProductListItem
-          produit={{
-            id: 3,
-            name: 'beurre salé',
-            img: 'https://dojwn62xby8qn.cloudfront.net/public/offer_presentations/slideshows/original/16389-1640171519.png?1640171519',
-            prix: 4.85,
-            stock: 2,
-          }}
-        />
+        {/* <View>
+          <ProductListItem
+            produit={{
+              id: 1,
+              name: 'coca zero 33cl',
+              img: 'https://www.bieres64-40.fr/wp-content/uploads/2022/12/cocazero33.png',
+              prix: 1.99,
+              stock: 6,
+            }}
+          />
+          <ProductListItem
+            produit={{
+              id: 2,
+              name: 'Kouign aman',
+              img: 'https://biscuiterie-lafabrique.fr/wp-content/uploads/2024/09/Kouign-amann-praline-rose-La-Fabrique-Chavagne-e1747217497199.png',
+              prix: 3.85,
+              stock: 0,
+            }}
+          />
+          <ProductListItem
+            produit={{
+              id: 3,
+              name: 'beurre salé',
+              img: 'https://dojwn62xby8qn.cloudfront.net/public/offer_presentations/slideshows/original/16389-1640171519.png?1640171519',
+              prix: 4.85,
+              stock: 2,
+            }}
+          />
+        </View> */}
+        <View style={{ borderTopWidth: 1, borderTopColor: 'black' }}>
+          <Text style={{ textAlign: 'center' }}>Counter : {counter}</Text>
+          <Button
+            title="Ajouter 1"
+            onPress={() => {
+              console.log(counter);
+              counter++;
+            }}
+          />
+        </View>
       </View>
     </SafeAreaProvider>
   );
