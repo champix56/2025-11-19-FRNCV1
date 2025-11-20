52c32ef7db84e8d33e1d0c2b394c9e9e69aae37c
champix56 <desorbaix@free.fr>
2025-11-21 00:51:49 +0100

    ajout forcer au cart pour debug du cart

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,36 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import stockReducer, { fetchProducts } from './stock';
-import cartReducer from './cart';
+import cartReducer, { addProductToCart } from './cart';
 const store = configureStore({
-  reducer: {stock:stockReducer, cart:cartReducer},
+  reducer: { stock: stockReducer, cart: cartReducer },
 });
-
-
-
-
 
 export default store;
 //typage du store(states et dispatch) invariable
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-
-
+let isCartDebugExectuted = false;
 store.subscribe(() => {
   console.log('Store updated : ', store.getState());
+
+  if (!isCartDebugExectuted && store.getState().stock.products.length > 0) {
+    isCartDebugExectuted = true;
+    store.dispatch(addProductToCart(store.getState().stock.products[0]));
+    store.dispatch(addProductToCart(store.getState().stock.products[0]));
+    store.dispatch(addProductToCart(store.getState().stock.products[1]));
+  }
 });
 
 store.dispatch(fetchProducts());
-
-// store.dispatch(addProductToStock(products[0]));
-// store.dispatch(addProductToStock(products[1]));
-// store.dispatch(addProductToStock(products[2])); 
-
-// store.dispatch(addProductToCart(products[0]));
-// store.dispatch(addProductToCart(products[0]));
-// store.dispatch(addProductToCart(products[2]));
-// store.dispatch(addProductToCart(products[2]));
-// store.dispatch(addProductToCart(products[2]));
-
-// store.dispatch(removeQuantityProductInCartById(products[2].id));
-
