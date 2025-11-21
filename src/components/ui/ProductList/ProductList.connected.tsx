b09378b43719f252892b440be69995010efed7d7
champix56 <desorbaix@free.fr>
2025-11-21 01:21:41 +0100

    connexion des composant avec react-redux

--- src/components/ui/ProductList/ProductList.connected.tsx
+++ src/components/ui/ProductList/ProductList.connected.tsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ProductListUnconnected from './ProductList';
-import store from '../../../store/store';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store/store';
 interface IProductListProps {
     height?: number;
   }
 const ProductList = (props:IProductListProps) => {
-  const [products, setproducts] = useState<Array<IProductListItem>>([]);
-  useEffect(() => {
-    setproducts(store.getState().stock.products);
-    store.subscribe(() => {
-      setproducts(store.getState().stock.products);
-    });
-  }, []);
+  const products=useSelector((state:RootState)=>state.stock.products);
   return (
     <ProductListUnconnected {...props} products={products} />
   );
