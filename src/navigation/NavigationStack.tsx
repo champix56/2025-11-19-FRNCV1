6cd195e54133988905ae84334e67a83565e80ffa
champix56 <desorbaix@free.fr>
2025-11-21 14:34:23 +0100

    composant specifique as header of stack.Screen

--- src/navigation/NavigationStack.tsx
+++ src/navigation/NavigationStack.tsx
@@ -4,21 +4,38 @@ import Cart from '../pages/CartView';
 import Products from '../pages/ListProducts';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from '../pages/Home';
+import Banner from '../components/ui/Banner/Banner';
 const Stack = createNativeStackNavigator();
 const NavigationStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{
-          headerShown: false,
-        }} />
-        <Stack.Screen name="Cart" component={Cart} options={{
-          title: 'Mon panier',
-          headerStyle: { backgroundColor: '#F7AB8E' },
-          headerTitleStyle: { color: 'white' },
-          headerTintColor: 'white',
-        }} />
-        <Stack.Screen name="Products" component={Products} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Cart"
+          component={Cart}
+          options={{
+            title: 'Mon panier',
+            headerStyle: { backgroundColor: '#F7AB8E' },
+            headerTitleStyle: { color: 'white' },
+            headerTintColor: 'white',
+          }}
+        />
+        <Stack.Screen
+          name="Products"
+          component={Products}
+          options={{
+            headerTitle: () => {
+              return <Banner text="Produits" />;
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
