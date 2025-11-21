d54c1d358ec185ea57203300fbb4b3c11572ad89
champix56 <desorbaix@free.fr>
2025-11-21 16:09:50 +0100

    ajout de boutton de navigation pour mettre en edition un produit

--- src/pages/ProductViewer.tsx
+++ src/pages/ProductViewer.tsx
@@ -1,15 +1,29 @@
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import React from 'react';
 import Product from '../components/ui/Products/Product';
 import { IProductItem } from '../interfaces/IProducts';
-import {StaticScreenProps} from '@react-navigation/native';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
+
 // interface IProductViewerProps {
 //   route:
 // }
-const ProductViewer = ({route}: StaticScreenProps<{product:IProductItem}>) => {
+const ProductViewer = ({
+  route,
+}: StaticScreenProps<{ product: IProductItem }>) => {
+  const n = useNavigation();
   return (
     <View>
       <Product product={route.params.product} onEditClick={() => {}} />
+
+      <Button
+        title="Editer"
+        onPress={() => {
+          //@ts-ignore
+          n.navigate('ProductEditor' as never, {
+            product: route.params.product,
+          });
+        }}
+      />
     </View>
   );
 };
