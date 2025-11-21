3197489714d65880539e4e28a15026e43a88bc37
champix56 <desorbaix@free.fr>
2025-11-21 01:22:01 +0100

    connexion des dispatch

--- src/components/ui/CartList/CartList.connected.tsx
+++ src/components/ui/CartList/CartList.connected.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import CartListUncconected from './CartList'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../../store/store'
-const CartList = (props:any) => {
-  const products= useSelector((state:RootState) => state.cart.products)
-  return (
-   <CartListUncconected products={products} {...props}/>
-  )
-}
+import React from 'react';
+import CartListUncconected from './CartList';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../store/store';
+import {
+  addProductToCart,
+  removeQuantityProductInCartById,
+} from '../../../store/cart';
 
-export default CartList
+const CartList = () => {
+  const products = useSelector((state: RootState) => state.cart.products);
+  const dispatch = useDispatch();
+  return (
+    <CartListUncconected
+      products={products}
+      addQuantityProductInCartById={prod => {
+        dispatch(addProductToCart(prod));
+      }}
+      removeQuantityProductInCartById={(id: number) => {
+        dispatch(removeQuantityProductInCartById(id));
+      }}
+    />
+  );
+};
+
+export default CartList;
